ba04f4da2e0b87d7b777d5ea806b6907b5be510f
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-10-12 23:55:41 +0530

    Vowel/Consonant

--- Python/day1.py
+++ Python/day1.py
@@ -4,3 +4,15 @@ if alph.lower( ) in vowels:
     print(alph + " is an vowel")
 else:
     print("consonant")
+
+
+#verifies that the input consists of exactly one character
+# alph = input("Enter an alphabet: ")
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# if len(alph) == 1 and alph.isalpha():
+#     if alph.lower() in vowels:
+#         print(alph + " is a vowel")
+#     else:
+#         print(alph + " is a consonant")
+# else:
+#     print("Please enter a valid single alphabet.")
