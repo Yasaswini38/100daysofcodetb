632a2d450ffb58bcddbd86a27eebd2c3af83ac43
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-10-15 19:03:52 +0530

    (x,y) in q=[1,2,3,4]

--- Python/day6.py
+++ Python/day6.py
@@ -11,3 +11,24 @@ elif(x<0 and y>0):
     print(f"({x},{y}) are in Q2")
 else:
     print(f"({x},{y})are Q4")
+
+# def quad(x, y):
+#     if x == 0 and y == 0:
+#         return "Origin"
+#     elif x == 0:
+#         return "Y-axis"
+#     elif y == 0:
+#         return "X-axis"
+    
+#     switch = {
+#         (True, True): "I Quadrant",    
+#         (False, True): "II Quadrant",  
+#         (False, False): "III Quadrant",
+#         (True, False): "IV Quadrant"   
+#     }
+#     return switch[(x > 0, y > 0)]
+    
+# x = int(input("Enter X Coord: "))
+# y = int(input("Enter Y Coord: "))
+# res = quad(x, y)
+# print(f"({x}, {y}) are in the {res}")
