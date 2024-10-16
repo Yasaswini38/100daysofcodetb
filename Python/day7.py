5d02a88a6f07b6b400adf9466ddf9b7c0b1d32c9
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-10-16 20:53:57 +0530

    days(month)

---
+++ Python/day7.py
@@ -0,0 +1,28 @@
+#Write a program to find Number of days in a given month of a given year
+def yrchk(year):
+    if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
+        return True
+    return False
+    
+def daizz(month, year):
+    mon31 = ["January", "March", "May", "July", "August", "October", "December"]
+    mon30 = ["April", "June", "September", "November"]
+    month = month.capitalize()
+    if month in mon31:
+        return 31
+    elif month in mon30:
+        return 30
+    elif month == "February":
+        if yrchk(year):
+            return 29  #leap
+        else:
+            return 28 #non leap
+    else:
+        return "Invalid month" 
+
+month = input("Enter the name of the month: ")
+year = int(input("Enter the year: "))
+days = daizz(month, year)
+print(f"{month} {year} has {days} days.")
+
+
