c0e695af040cdf9eac4aa4858d5732288e78d439
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-10-28 19:11:01 +0530

    fractionsss

---
+++ Python/day18.py
@@ -0,0 +1,8 @@
+from fractions import Fraction
+
+frac1 = Fraction(input("Enter the first fraction (e.g., 1/2): "))
+frac2 = Fraction(input("Enter the second fraction (e.g., 1/3): "))
+
+result = frac1 + frac2
+
+print("The sum of", frac1, "and", frac2, "is:", result)
