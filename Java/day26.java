6de7bf921cbfcb2aa1ec968f79c6c9e7f6e98ea4
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-11-08 05:56:39 +0530

    handshakes

--- Java/day26.java
+++ Java/day26.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
-
-public class HandshakeCalculator {
-    public static int calculateHandshakes(int n) {
-        return n * (n - 1) / 2; 
+/*Get the number of people in the room as input from the user. 
+Then calculate the maximum number of handshakes possible among that people.
+For e.g. If there are N people in the room then the first person 
+has to shake hand with N-1 people and second person has to shake hand 
+with N-1-1 people i.e., N-2 handshakes are possible. Thus, it goes on.
+So total hand shakes = N-1 + N-2 + N-3 +………+1 + 0 */
+public class handshakes {
+    public static int totalHandshakes(int n) {
+        return (n * (n- 1)) / 2;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of people in the room: ");
-        int n = scanner.nextInt();
-        
-        if (n < 0) {
-            System.out.println("The number of people cannot be negative.");
-        } else {
-            int totalHandshakes = calculateHandshakes(n);
-            System.out.println("The maximum number of handshakes possible among " + n + " people is: " + totalHandshakes);
-        }
+        int N = scanner.nextInt();
+        System.out.println("The maximum number of handshakes possible is: " 
+        + totalHandshakes(N));
+        scanner.close();
     }
 }
+
