9428befac7b909ef8c2a718758ec329e1fb00303
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-11-09 19:30:00 +0530

    doublewithout op

---
+++ Python/day27.py
@@ -0,0 +1,7 @@
+#Write a program to find the double of the given number without using arithmetic operator
+
+def double(n):
+    return n<<1
+
+n=int(input("Enter number to be doubled "))
+print(double(n))    
