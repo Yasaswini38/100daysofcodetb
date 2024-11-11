9dc030f4bc4d913b2c38d4a8a62ebea759a7093c
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-11-11 05:43:01 +0530

    strconct

--- Python/day28.py
+++ Python/day28.py
@@ -1,13 +1,6 @@
-def palindrome(n):
-    rev=0
-    ori=n
-    while(n!=0):
-        rev=rev*10+(n%10)
-        n//=10
-    return rev==ori
+a = input("Enter the first string: ")
+b= input("Enter the second string: ")
 
-n=int(input("Enter a number "))
-if(palindrome(n)):
-    print("A palindromee")
-else:
-    print("not onnee")
+result = a+b
+
+print("Concatenated String:", result)
