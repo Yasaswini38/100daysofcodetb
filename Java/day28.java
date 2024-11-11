4b2cf182988796549dfb2ca249ce81640d9f7076
Yasaswini Padamati <119612379+Yasaswini38@users.noreply.github.com>
2024-11-11 05:43:48 +0530

    concat

---
+++ Java/day28.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class conct {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String firstString = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String secondString = scanner.nextLine();
+        String result = firstString + secondString;
+        System.out.println("Concatenated String: " + result);
+        scanner.close();
+    }
+
+
+}
